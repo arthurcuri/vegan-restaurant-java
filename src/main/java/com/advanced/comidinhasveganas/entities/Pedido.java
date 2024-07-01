e080ce9e69a093a4811d51e130d9d4758a128a0a
Arthur Kramberger <arthur1curi@gmail.com>
2024-07-01 16:51:28 -0300

    add precoStrategy and related classes

--- src/main/java/com/advanced/comidinhasveganas/entities/Pedido.java
+++ src/main/java/com/advanced/comidinhasveganas/entities/Pedido.java
@@ -3,6 +3,12 @@ package com.advanced.comidinhasveganas.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.advanced.comidinhasveganas.converters.PrecoStrategyConverter;
+import com.advanced.comidinhasveganas.strategy.PrecoFechadoStrategy;
+import com.advanced.comidinhasveganas.strategy.PrecoNormalStrategy;
+import com.advanced.comidinhasveganas.strategy.PrecoStrategy;
+
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,67 +17,153 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+/**
+ * Representa um pedido no restaurante.
+ * Cada pedido possui um ID único, uma lista de itens, um tipo e um preço total.
+ */
 @Entity
 @Table(name = "tb_pedidos")
 public class Pedido {
 
+  /**
+   * Identificador único do pedido.
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  /**
+   * Lista de itens do pedido.
+   */
   @OneToMany
   @JoinColumn(name = "pedido_id")
   private List<ItemPedido> itens = new ArrayList<>();
 
+  /**
+   * Tipo do pedido (normal ou fechado).
+   */
   private String tipoPedido;
 
+  /**
+   * Preço total do pedido.
+   */
   private double precoTotal;
 
+  /**
+   * Estratégia de cálculo do preço do pedido.
+   */
+  @Convert(converter = PrecoStrategyConverter.class)
+  private PrecoStrategy precoStrategy;
+
+  /**
+   * Construtor padrão.
+   */
   public Pedido() {
   }
 
+  /**
+   * Construtor para inicializar o pedido com um tipo específico.
+   * 
+   * @param tipoPedido Tipo do pedido.
+   */
   public Pedido(String tipoPedido) {
     setTipoPedido(tipoPedido);
   }
 
+  /**
+   * Obtém o identificador único do pedido.
+   * 
+   * @return Identificador único do pedido.
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Obtém a lista de itens do pedido.
+   * 
+   * @return Lista de itens do pedido.
+   */
   public List<ItemPedido> getItens() {
     return itens;
   }
 
+  /**
+   * Obtém o tipo do pedido.
+   * 
+   * @return Tipo do pedido.
+   */
   public String getTipoPedido() {
     return tipoPedido;
   }
 
+  /**
+   * Define o tipo do pedido.
+   * 
+   * @param tipoPedido Tipo do pedido a ser definido.
+   */
   public void setTipoPedido(String tipoPedido) {
     this.tipoPedido = tipoPedido;
+    definirStrategy();
   }
 
+  /**
+   * Obtém o preço total do pedido.
+   * 
+   * @return Preço total do pedido.
+   */
   public double getPrecoTotal() {
     return precoTotal;
   }
 
+  /**
+   * Adiciona um item ao pedido.
+   * 
+   * @param item Item a ser adicionado.
+   */
   public void addItem(ItemPedido item) {
     itens.add(item);
   }
 
+  /**
+   * Adiciona uma lista de itens ao pedido.
+   * 
+   * @param itens Lista de itens a serem adicionados.
+   */
   public void addItens(List<ItemPedido> itens) {
     this.itens.addAll(itens);
   }
 
-  public void setPrecoTotal() {
+  /**
+   * Define o preço total do pedido com base no tipo de pedido.
+   * Para pedidos do tipo "normal", o preço total é a soma dos subtotais dos
+   * itens.
+   * Para pedidos do tipo "fechado", o preço total é fixo em 32.0.
+   * 
+   * @throws IllegalArgumentException Se o tipo de pedido for desconhecido.
+   */
+  private void definirStrategy() {
     if (tipoPedido.equalsIgnoreCase("normal")) {
-      precoTotal = itens.stream().mapToDouble(ItemPedido::getSubTotal).sum();
+      this.precoStrategy = new PrecoNormalStrategy(itens);
     } else if (tipoPedido.equalsIgnoreCase("fechado")) {
-      precoTotal = 32.0;
+      this.precoStrategy = new PrecoFechadoStrategy();
     } else {
       throw new IllegalArgumentException("Tipo de pedido desconhecido.");
     }
   }
 
+  /**
+   * Calcula o preço total do pedido com base na estratégia de cálculo de preço.
+   */
+  public void setPrecoTotal() {
+    precoTotal = precoStrategy.calcularPreco();
+  }
+
+  /**
+   * Retorna uma representação em string do pedido.
+   * 
+   * @return Uma representação em string do pedido.
+   */
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
--- src/main/java/com/advanced/comidinhasveganas/entities/Requisicao.java
+++ src/main/java/com/advanced/comidinhasveganas/entities/Requisicao.java
@@ -12,6 +12,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+/**
+ * A classe Requisicao representa um pedido feito por um cliente em um
+ * restaurante.
+ * Ela inclui informações sobre o cliente, mesa, quantidade de pessoas, status
+ * do pedido,
+ * horários de início e fim, total da conta, total por pessoa e os pedidos
+ * realizados.
+ */
 @Entity
 @Table(name = "tb_requisicoes")
 public class Requisicao {
@@ -46,122 +54,267 @@ public class Requisicao {
   @JoinColumn(name = "requisicao_id")
   private List<Pedido> pedidos;
 
+  /**
+   * Construtor padrão.
+   */
   public Requisicao() {
   }
 
+  /**
+   * Construtor que inicializa a Requisicao com um cliente e quantidade de
+   * pessoas.
+   *
+   * @param cliente           O cliente que fez a requisição.
+   * @param quantidadePessoas A quantidade de pessoas na mesa.
+   */
   public Requisicao(Cliente cliente, Integer quantidadePessoas) {
     setCliente(cliente);
     setQuantidadePessoas(quantidadePessoas);
   }
 
+  /**
+   * Retorna o ID da requisição.
+   *
+   * @return O ID da requisição.
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Retorna o cliente que fez a requisição.
+   *
+   * @return O cliente.
+   */
   public Cliente getCliente() {
     return cliente;
   }
 
+  /**
+   * Define o cliente que fez a requisição.
+   *
+   * @param cliente O cliente a ser definido.
+   */
   public void setCliente(Cliente cliente) {
     this.cliente = cliente;
   }
 
+  /**
+   * Retorna a quantidade de pessoas na mesa.
+   *
+   * @return A quantidade de pessoas.
+   */
   public Integer getQuantidadePessoas() {
     return quantidadePessoas;
   }
 
+  /**
+   * Define a quantidade de pessoas na mesa.
+   *
+   * @param quantidadePessoas A quantidade de pessoas a ser definida.
+   */
   public void setQuantidadePessoas(Integer quantidadePessoas) {
     this.quantidadePessoas = quantidadePessoas;
   }
 
+  /**
+   * Retorna a mesa associada à requisição.
+   *
+   * @return A mesa.
+   */
   public Mesa getMesa() {
     return mesa;
   }
 
+  /**
+   * Define a mesa associada à requisição.
+   *
+   * @param mesa A mesa a ser definida.
+   */
   public void setMesa(Mesa mesa) {
     this.mesa = mesa;
   }
 
+  /**
+   * Retorna se a requisição foi atendida.
+   *
+   * @return Verdadeiro se a requisição foi atendida, falso caso contrário.
+   */
   public Boolean getIsAtendida() {
     return isAtendida;
   }
 
+  /**
+   * Define se a requisição foi atendida.
+   *
+   * @param isAtendida Verdadeiro se a requisição foi atendida, falso caso
+   *                   contrário.
+   */
   public void setIsAtendida(Boolean isAtendida) {
     this.isAtendida = isAtendida;
   }
 
+  /**
+   * Define a requisição como atendida.
+   */
   public void setAtendida() {
     this.isAtendida = true;
   }
 
+  /**
+   * Retorna se a requisição foi finalizada.
+   *
+   * @return Verdadeiro se a requisição foi finalizada, falso caso contrário.
+   */
   public Boolean getIsFinalizada() {
     return isFinalizada;
   }
 
+  /**
+   * Define se a requisição foi finalizada.
+   *
+   * @param isFinalizada Verdadeiro se a requisição foi finalizada, falso caso
+   *                     contrário.
+   */
   public void setIsFinalizada(Boolean isFinalizada) {
     this.isFinalizada = isFinalizada;
   }
 
+  /**
+   * Define a requisição como finalizada.
+   */
   public void setFinalizada() {
     this.isFinalizada = true;
   }
 
+  /**
+   * Retorna a data e hora de início da requisição.
+   *
+   * @return A data e hora de início.
+   */
   public LocalDateTime getDataHoraInicio() {
     return dataHoraInicio;
   }
 
+  /**
+   * Define a data e hora de início da requisição.
+   *
+   * @param dataHoraInicio A data e hora de início a ser definida.
+   */
   public void setDataHoraInicio(LocalDateTime dataHoraInicio) {
     this.dataHoraInicio = dataHoraInicio;
   }
 
+  /**
+   * Define a data e hora de início da requisição como o momento atual.
+   */
   public void setDataHoraInicioLocal() {
     this.dataHoraInicio = LocalDateTime.now();
   }
 
+  /**
+   * Retorna a data e hora de fim da requisição.
+   *
+   * @return A data e hora de fim.
+   */
   public LocalDateTime getDataHoraFim() {
     return dataHoraFim;
   }
 
+  /**
+   * Define a data e hora de fim da requisição.
+   *
+   * @param dataHoraFim A data e hora de fim a ser definida.
+   */
   public void setDataHoraFim(LocalDateTime dataHoraFim) {
     this.dataHoraFim = dataHoraFim;
   }
 
+  /**
+   * Define a data e hora de fim da requisição como o momento atual.
+   */
   public void setDataHoraFimLocal() {
     this.dataHoraFim = LocalDateTime.now();
   }
 
+  /**
+   * Retorna o total da conta.
+   *
+   * @return O total da conta.
+   */
   public Double getTotalConta() {
     return totalConta;
   }
 
+  /**
+   * Define o total da conta.
+   *
+   * @param totalConta O total da conta a ser definido.
+   */
   public void setTotalConta(Double totalConta) {
     this.totalConta = totalConta;
   }
 
+  /**
+   * Retorna o total por pessoa.
+   *
+   * @return O total por pessoa.
+   */
   public Double getTotalPorPessoa() {
     return totalPorPessoa;
   }
 
+  /**
+   * Define o total por pessoa.
+   *
+   * @param totalPorPessoa O total por pessoa a ser definido.
+   */
   public void setTotalPorPessoa(Double totalPorPessoa) {
     this.totalPorPessoa = totalPorPessoa;
   }
 
+  /**
+   * Retorna a lista de pedidos associados à requisição.
+   *
+   * @return A lista de pedidos.
+   */
   public List<Pedido> getPedidos() {
     return pedidos;
   }
 
+  /**
+   * Define a lista de pedidos associados à requisição.
+   *
+   * @param pedidos A lista de pedidos a ser definida.
+   */
   public void setPedidos(List<Pedido> pedidos) {
     this.pedidos = pedidos;
   }
 
+  /**
+   * Adiciona um pedido à lista de pedidos.
+   *
+   * @param pedido O pedido a ser adicionado.
+   */
   public void addPedido(Pedido pedido) {
+    pedido.setPrecoTotal(); // Certifique-se de calcular o preço total ao adicionar o pedido
     pedidos.add(pedido);
   }
 
+  /**
+   * Remove um pedido da lista de pedidos.
+   *
+   * @param pedido O pedido a ser removido.
+   */
   public void removePedido(Pedido pedido) {
     pedidos.remove(pedido);
   }
 
+  /**
+   * Inicia a requisição associando-a a uma mesa e marcando o horário de início.
+   *
+   * @param mesa A mesa a ser associada.
+   */
   public void iniciarRequisicao(Mesa mesa) {
     setMesa(mesa);
     setDataHoraInicioLocal();
@@ -169,6 +322,10 @@ public class Requisicao {
     mesa.ocupar();
   }
 
+  /**
+   * Finaliza a requisição marcando o horário de fim, calculando os totais e
+   * liberando a mesa.
+   */
   public void finalizarRequisicao() {
     setDataHoraFimLocal();
     setFinalizada();
@@ -176,6 +333,9 @@ public class Requisicao {
     calcularTotais();
   }
 
+  /**
+   * Calcula os totais da conta e por pessoa, incluindo um acréscimo de 10%.
+   */
   public void calcularTotais() {
     Double tc = pedidos.stream().mapToDouble(Pedido::getPrecoTotal).sum() * 1.1;
 
@@ -185,6 +345,9 @@ public class Requisicao {
     setTotalPorPessoa(tp);
   }
 
+  /**
+   * Cancela a requisição.
+   */
   public void cancelarRequisicao() {
     isFinalizada = true;
   }
